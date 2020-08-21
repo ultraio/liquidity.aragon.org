8cb54f3d6745ef42f568fdc5b02b82f2a81c2bec
Olha Shekhovtsova <olha.shekhovtsova@ultra.io>
2020-08-21 11:32:12 +0300

    Add styles for success Info component

--- components/Info/Info.js
+++ components/Info/Info.js
@@ -3,6 +3,7 @@ import React from 'react'
 const MODES = new Map([
   ['info', { background: '', color: 'rgba(255, 255, 255, 0.7)' }],
   ['warning', { background: 'rgba(179, 173, 255, 0.1)', color: '#C5ABFF'}],
+  ['success', { background: 'rgba(144, 242, 154, 0.1)', color: '#8CE895'}],
 ])
 
 export default function Info({
