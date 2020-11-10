548a1be41362900a06a8d66fbbcbe2d3cca6d5f5
olhashekhovtsova <63141632+olhashekhovtsova@users.noreply.github.com>
2020-11-10 11:38:48 +0200

    [UL-60] [should be merged only in the day, when liquidity program would be ended] Add banner for liquidity program ending, leave withdraw only functionality (#10)
    * Add ending banner (#10)
    
    * Update ending banner copy (#11)
    
    * Update ending banner copy
    
    * Semantic HTML improvements
    
    * Margin fix
    
    Co-authored-by: Enrique Ortiz <hi@enriqueortiz.dev>
    
    * Rewards: Update banner for rewards ending, leave withdraw only functionality (#12)
    
    * Update banner for rewards ending, leave withdraw only functionality
    
    * Remove unneeded prop
    
    * rewards -> Liquidity
    
    * Clarify withdraw functionality
    
    * Fix Focus Ring on Chrome
    
    * Update styles for diffent screen sizes, add 'Inter bold' font, etc
    
    * Remove unused styles
    
    * Remove parts that aren't work (socials, etc)
    
    * Refactor the components
    
    * Refactor navigation
    
    * Refactor the footer
    
    * Remove unused styles
    
    * Update styles
    
    * Fix position of the account info
    
    * Fix styles for the banner
    
    Co-authored-by: Enrique Ortiz <enrique@aragon.one>
    Co-authored-by: Jorge Izquierdo <jorge@aragon.one>
    Co-authored-by: Enrique Ortiz <hi@enriqueortiz.dev>

--- components/StakeModule/StakeModule.js
+++ components/StakeModule/StakeModule.js
@@ -90,7 +90,7 @@ function useConvertInputs() {
 }
 
 export default function StakeModule() {
-  const [activeKey, setActiveKey] = useState(0)
+  const [activeKey, setActiveKey] = useState(1)
   const [disabled, setDisabled] = useState(false)
   const [notification, setNotification] = useState('')
 
@@ -236,7 +236,6 @@ export default function StakeModule() {
           </div>
           <ButtonGroup
             activeKey={activeKey}
-            disabled={disabled}
             elements={SECTIONS}
             isCompact={isCompact}
             onSetActiveKey={setActiveKey}
