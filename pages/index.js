1af7118bc129f4ac4b69d8f23acbd676b9ec47f9
olhashekhovtsova <63141632+olhashekhovtsova@users.noreply.github.com>
2020-11-10 11:38:48 +0200

    [UL-60] [should be merged only in the day, when liquidity program would be ended] Add banner for liquidity program ending, leave withdraw only functionality (#10)
    * Add ending banner (#10)
    
    * Update ending banner copy (#11)
    
    * Update ending banner copy
    
    * Semantic HTML improvements
    
    * Margin fix
    
    Co-authored-by: Enrique Ortiz <hi@enriqueortiz.dev>
    
    * Rewards: Update banner for rewards ending, leave withdraw only functionality (#12)
    
    * Update banner for rewards ending, leave withdraw only functionality
    
    * Remove unneeded prop
    
    * rewards -> Liquidity
    
    * Clarify withdraw functionality
    
    * Fix Focus Ring on Chrome
    
    * Update styles for diffent screen sizes, add 'Inter bold' font, etc
    
    * Remove unused styles
    
    * Remove parts that aren't work (socials, etc)
    
    * Refactor the components
    
    * Refactor navigation
    
    * Refactor the footer
    
    * Remove unused styles
    
    * Update styles
    
    * Fix position of the account info
    
    * Fix styles for the banner
    
    Co-authored-by: Enrique Ortiz <enrique@aragon.one>
    Co-authored-by: Jorge Izquierdo <jorge@aragon.one>
    Co-authored-by: Enrique Ortiz <hi@enriqueortiz.dev>

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import { useViewport } from 'use-viewport'
 import StakeModule from 'components/StakeModule/StakeModule'
 import Header from 'components/RibbonModule/Header'
 import Footer from 'components/RibbonModule/Footer'
+import Info from 'components/Info/Info'
+
+const SMALL_BREAKPOINT = 415
 
 const GQL_ENDPOINT = `${process.env.WEBSITE_BACKEND_URL}/graphql`
 
@@ -11,7 +14,7 @@ export default () => {
   const { below } = useViewport()
   const [socials, setSocials] = useState([])
   const [isCompact, setIsCompact] = useState(false)
-  const smallLayout = below(415)
+  const smallLayout = below(SMALL_BREAKPOINT)
   useEffect(() => {
     setTimeout(() => {
       setIsCompact(smallLayout)
@@ -61,7 +64,7 @@ export default () => {
         css={`
           position: relative;
           min-height: 800px;
-          padding: 6em 0;
+          padding: 3em 0 6rem;
           background: transparent;
           display: flex;
           flex-direction: column;
@@ -69,9 +72,42 @@ export default () => {
           justify-content: flex-start;
         `}
       >
+        <Banner />
         <StakeModule />
       </div>
       <Footer socials={socials} />
+    </div>
+  )
+}
+
+function Banner() {
+  const { below } = useViewport()
+  const [isCompact, setIsCompact] = useState(false)
+  const smallLayout = below(SMALL_BREAKPOINT)
+  useEffect(() => {
+    setTimeout(() => {
+      setIsCompact(smallLayout)
+    }, 0)
+  }, [smallLayout])
+
+  return (
+    <div
+      className="l-12"
+      css={`
+        width: 100%;
+        max-width: 940px;
+        padding: 0 32px;
+        margin-bottom: 50px;
+      `}>
+      <Info
+        mode="mandatory-warning"
+        padding="16"
+        isCompact={isCompact}
+      >
+        {isCompact
+          ? 'The first UOS liquidity program has ended.'
+          : 'The first UOS Liquidity Rewards program has ended.'}
+      </Info>
     </div>
   )
 }
